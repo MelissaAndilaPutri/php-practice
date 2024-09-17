4fc81d6589be7b1fc70285606445f3dd4c898b65
MelissaAndilaPutri <melissaandilaputri@gmail.com>
2024-09-17 14:34:32 +0700

    Bab4: Struktur kontrol (nilai ujian, fizzbuzz, dan penjumlahan)

---
+++ Bab4_StrukturKontrol/nilai_ujian.php
@@ -0,0 +1,14 @@
+<?php
+$nilai = 95;
+if ($nilai >= 95) {
+    echo "Nilai: A";
+} elseif ($niali >= 85) {
+    echo "Nilai: B";
+} elseif ($nilai >= 75) {
+    echo "Nilai: C";
+} elseif ($nilai >= 65) {
+    echo "Nilai: D";
+} else {
+    echo "Nilai: F";
+}
+?>
