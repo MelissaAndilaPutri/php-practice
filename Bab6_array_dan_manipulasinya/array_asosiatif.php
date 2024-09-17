fd86a44fb696ddeebf3e9af64045d9dd4eba1e6d
MelissaAndilaPutri <melissaandilaputri@gmail.com>
2024-09-17 14:41:10 +0700

    Bab6: Array Numerik, asosiatif dan merge

---
+++ Bab6_array_dan_manipulasinya/array_asosiatif.php
@@ -0,0 +1,5 @@
+<?php
+$produk = array("nama" => "Laptop", "harga" => 5000000, "stok" => 10);
+$produk['stok'];
+echo "Nama Produk: " . $produk['nama'] . ", Harga: " . $produk['harga'] . ", Stok: " . $produk['stok'];
+?>
---
+++ Bab6_array_dan_manipulasinya/array_buah.php
@@ -0,0 +1,6 @@
+<?php
+$buah = array("Apel", "Jeruk", "Pisang");
+foreach ($buah as $b) {
+    echo "$b\n";
+}
+?>
